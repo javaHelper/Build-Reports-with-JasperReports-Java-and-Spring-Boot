b2902f3907d40bcc780e8583e08157eb3b429b4d
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-08-13 16:16:19 +0530

    Sorting Table Data By Column

---
+++ 1.4sorting-table-data-by-column/src/main/java/com/example/demo/FirstReportApplication.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.base.JRBaseTextField;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+public class FirstReportApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstReportApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("studentName", "John");
+
+		String filePath = ResourceUtils.getFile("classpath:FirstReport.jrxml")
+				.getAbsolutePath();
+
+		// A data source implementation that wraps a collection of JavaBean objects.
+		//It is common to access application data through object persistence layers like EJB, Hibernate, or JDO.
+		// Such applications may need to generate reports using data they already have available as arrays or
+		// collections of in-memory JavaBean objects.
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(this.students());
+		JasperReport report = JasperCompileManager.compileReport(filePath);
+
+		JRBaseTextField textField = (JRBaseTextField) report.getTitle().getElementByKey("name");
+		textField.setForecolor(Color.RED);
+
+		JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
+		JasperExportManager.exportReportToPdfFile(print, "FirstReport.pdf");
+		System.out.println("Report Created...");
+	}
+
+	private List<Student> students(){
+		List<Student> students = new ArrayList<>();
+		students.add(Student.builder().id(1L).firstName("John").lastName("Doe").city("Nagpur").street("AAA Street").build());
+		students.add(Student.builder().id(2L).firstName("Jane").lastName("Doe").city("Yavatmal").street("BBB Street").build());
+		students.add(Student.builder().id(3L).firstName("Mike").lastName("Doe").city("Akola").street("CCC Street").build());
+		students.add(Student.builder().id(4L).firstName("Matt").lastName("Doe").city("Amravati").street("DDD Street").build());
+		students.add(Student.builder().id(5L).firstName("Victoria").lastName("Doe").city("Wardha").street("EEE Street").build());
+		return students;
+	}
+}
---
+++ 1.4sorting-table-data-by-column/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Student {
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String street;
+	private String city;
+}
